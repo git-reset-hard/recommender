076619f0ed6b6d2dcf175c7b592784a385294d1a
dulynoded <administrator@administrators-Mac-Pro.local>
2017-10-24 17:33:41 -0700

    (feat) Add insertUser db function

--- database/index.js
+++ database/index.js
@@ -1,40 +1,39 @@
-const fake = require('../example_data/users.js');
+// const fake = require('../example_data/users.js');
+const faker = require('faker');
+const shortid = require('shortid');
+
 const neo4j = require('neo4j-driver').v1;
+const neo = require('../config/neo.js')
+const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", neo.neoPassword));
+const session = driver.session();
 
-const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "yelp"));
+const insertUser = `CREATE (n:User
+  {
+  customer_id: '${shortid.generate()}',
+  star_pref: ${Math.random()},
+  distance_pref: ${Math.random()},
+  price_pref: ${Math.random()},
+  openness: ${Math.random()},
+  hometown_latitude: ${faker.address.latitude()},
+  hometown_longitude: ${faker.address.longitude()},
+  personality: '[${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}]',
+  needs: '[${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}]',
+  values: '[${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()},${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}]'
+  }
+)`
 
-
-var session = driver.session();
-
-// Run a Cypher statement, reading the result in a streaming manner as records arrive:
-session
-  .run("CREATE (n:Person { name: 'Andres', title: 'Developer' })")
-  .subscribe({
-    onNext: function (record) {
-      console.log(record.get('name'));
-    },
-    onCompleted: function () {
+  session
+    .run(insertUser)
+    .then(function (result) {
+      result.records.forEach(function (record) {
+        console.log(record.get('name'));
+      });
       session.close();
-    },
-    onError: function (error) {
-      console.log(error);
-    }
-  });
-driver.close();
+      driver.close();
 
-// {
-//   "props" : [ {
-//     "name" : "Andres",
-//     "position" : "Developer"
-//   }, {
-//     "name" : "Michael",
-//     "position" : "Developer"
-//   } ]
-// }
-// UNWIND $props AS map
-// CREATE (n)
-// SET n = map
-// const populateUsers =
-// "UNWIND $users AS map \
-// CREATE (n) \
-// SET n = map";
+    })
+    .catch(function (error) {
+      console.log(error);
+      driver.close();
+
+    });
