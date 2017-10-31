dacaf380e0843ca05f843c4583a9ed2b1497a57e
dulynoded <administrator@administrators-Mac-Pro.local>
2017-10-31 16:01:45 -0700

    (refactor) Seed db with script, add getList to db

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const db = require('../database/index');
+const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/list', (req, res) => {
-  const {user_id, location, category} = req.query;
-  // get recommended list from db
-  // if err res.sendStatus(500);
-  // else res.json(data)
-
+  // const {user_id, location, category} = req.query;
+  db.getList('joe').then(result => {
+    db.session.close();
+    const singleRecord = result.records[0];
+    const createdNodeId = singleRecord.get(0);
+    console.log('single record', singleRecord);
+    console.log('Matched created node with id: ' + createdNodeId);
+    res.json(createdNodeId);
+  });
 })
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
